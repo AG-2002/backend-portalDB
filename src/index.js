1f440ff0ae3d5a6c153510ac4f78f961790aea8f
Ankit Gupta <ankitgupta310802@gmail.com>
2025-01-11 04:27:38 +0530

    rewrite a base route

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use("/api/employee", employeeRoute);
 
 app.use(error);
 
-app.get("/",(rq,res)=>res.send("Express on Vercel"));
+app.get("/",(req,res)=>res.send("Express on Vercel"));
 
 app.listen(process.env.PORT, () => {
   console.log("listening at 8080");
