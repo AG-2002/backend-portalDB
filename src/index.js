3a863fda198a743cab1f376d668a6103d0a3aec8
Ankit Gupta <ankitgupta310802@gmail.com>
2025-01-15 18:09:02 +0530

    add api

--- src/index.js
+++ src/index.js
@@ -29,12 +29,7 @@ process.on("unhandledRejection", (ex) => {
 winston.add(new winston.transports.File({ filename: "logfile.log" }));
 winston.add(new winston.transports.MongoDB({ db: process.env.DB_URL }));
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_PROD_URL,
-    credentials: true,
-  })
-);
+app.use(cors({origin: process.env.FRONTEND_PROD_URL,credentials: true,}));
 app.use(express.json());
 
 app.use("/api/admin", adminRoute);
