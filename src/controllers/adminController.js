a4eb8896ef3517843e478db3a4d2e8cf94531e96
Ankit Gupta <ankitgupta310802@gmail.com>
2025-01-15 23:02:22 +0530

    .

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -13,7 +13,6 @@ exports.loginAdmin = async (req, res) => {
   const token = admin.generateToken();
 
   res.cookie("token", token, {
-    withCredentials: true,
     httpOnly: true,
     sameSite: "None",
     secure: true,
