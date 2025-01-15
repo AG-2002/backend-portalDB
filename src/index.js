5b5f927155f0310c2d2bd19a93c80c8a55b324a8
Ankit Gupta <ankitgupta310802@gmail.com>
2025-01-16 00:20:31 +0530

    .

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const employeeRoute = require("./routes/employeeRoute.js");
 
 const app = express();
 
+const allowedOrigins = [process.env.FRONTEND_PROD_URL, process.env.FRONTEND_DEV_URL];
+
 process.on("uncaughtException", (ex) => {
   winston.error(ex.message, ex);
   process.exit(1);
@@ -23,7 +25,8 @@ winston.exceptions.handle(
     format: winston.format.combine(
       winston.format.colorize(),
       winston.format.prettyPrint()
-    )}),
+    ),
+  }),
   new winston.transports.File({ filename: "uncaughtExceptions.log" })
 );
 process.on("unhandledRejection", (ex) => {
@@ -33,12 +36,21 @@ process.on("unhandledRejection", (ex) => {
 winston.add(new winston.transports.File({ filename: "logfile.log" }));
 winston.add(new winston.transports.MongoDB({ db: process.env.DB_URL }));
 
-app.use(cors({
-  origin: process.env.FRONTEND_PROD_URL,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // If `origin` is in the allowed list or is undefined (e.g., non-browser requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin); // Set the origin dynamically
+      } else {
+        callback(new Error("Not allowed by CORS")); // Reject the request
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/api/admin", adminRoute);
