a2c4de8a7b921738b8d7c4601719ca8ab6f0515d
Ankit Gupta <ankitgupta310802@gmail.com>
2025-01-16 08:26:14 +0530

    .

--- src/index.js
+++ src/index.js
@@ -13,32 +13,32 @@ const error = require("./middleware/error.js");
 
 const app = express();
 
-process.on("uncaughtException", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
+// process.on("uncaughtException", (ex) => {
+//   winston.error(ex.message, ex);
+//   process.exit(1);
+// });
 
-winston.exceptions.handle(
-  new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.prettyPrint()
-    ),
-  }),
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
+// winston.exceptions.handle(
+//   new winston.transports.Console({
+//     format: winston.format.combine(
+//       winston.format.colorize(),
+//       winston.format.prettyPrint()
+//     ),
+//   }),
+//   new winston.transports.File({ filename: "uncaughtExceptions.log" })
+// );
+// process.on("unhandledRejection", (ex) => {
+//   throw ex;
+// });
 
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(new winston.transports.MongoDB({ db: process.env.DB_URL }));
+// winston.add(new winston.transports.File({ filename: "logfile.log" }));
+// winston.add(new winston.transports.MongoDB({ db: process.env.DB_URL }));
 
 app.use(express.json());
 app.use(
   cors({
-    origin:[process.env.FRONTEND_PROD_URL],
-    credentials:true,
+    origin: [process.env.FRONTEND_PROD_URL],
+    credentials: true,
   })
 );
 
