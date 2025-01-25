49d8f42ae74a5f5826892bc9014ede1c5fbfc790
Ankit Gupta <ankitgupta310802@gmail.com>
2025-01-25 10:14:12 +0530

    fix multer

--- src/routes/employeeRoute.js
+++ src/routes/employeeRoute.js
@@ -4,9 +4,9 @@ const upload = require('../config/multer-config');
 const uploadToCloudinary = require('../middleware/uploadToCloudinary');
 const {addEmployee,getAllEmployees,updateEmployee,deleteEmployee} = require('../controllers/employeeController');
 
-router.post("/create",upload.single('image'),uploadToCloudinary,addEmployee);
+router.post("/create",upload.single('url'),uploadToCloudinary,addEmployee);
 router.get('/listing', getAllEmployees);
-router.put('/:id',upload.single('image'),uploadToCloudinary, updateEmployee);
+router.put('/:id',upload.single('url'),uploadToCloudinary, updateEmployee);
 router.delete('/:id',deleteEmployee)
 
 module.exports = router;
